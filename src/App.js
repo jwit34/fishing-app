d9cd43e49fb65f0f93230614ae45c952d14ffbbe
Jacob Whitwell <jacob.whitwell34@gmail.com>
2024-08-15 16:25:38 -0500

    aa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
